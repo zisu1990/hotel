5908b5c0ade1898586a49123a56d775f6d23fbfb
lili <865609424@qq.com>
2020-11-27 14:26:38 +0800

    aa

--- src/views/Booking/Booking.vue
+++ src/views/Booking/Booking.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container>
     <el-main>
-      <el-form ref="BookingForm" :model="BookingForm" :rules="BookingRef" label-width="100px">
+      <el-form   :model="BookingForm"   >
         <el-row>
           <el-col :span="8">
             <el-form-item label="预订时间段：">
@@ -15,45 +15,58 @@
             </el-form-item>
           </el-col>
           <el-col :span="6">
-            <el-form-item label="姓名：">
+            <el-form-item>
               <el-input
                 v-model="BookingForm.name"
-                placeholder="请输入姓名"
+                placeholder="请输入姓名/手机号"
                 clearable
                 :style="{width: '100%'}"
               ></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span="6">
-            <el-form-item label="电话：">
-              <el-input
-                v-model="BookingForm.tel"
-                placeholder="请输入联系电话"
-                clearable
-                :style="{width: '100%'}"
-              ></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="4">
+
+          <el-col :span="5">
             <el-button type="primary">查询</el-button>
             <el-button type="success">查询所有房间的预订</el-button>
           </el-col>
         </el-row>
       </el-form>
 
-      <el-table :data="BookingTableData" style="width: 100%" border>
-        <el-table-column prop="roomNum" label="房间号" width="80" align="center"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="100" align="center"></el-table-column>
-        <el-table-column prop="sex" label="性别" width="80" align="center"></el-table-column>
-        <el-table-column prop="cardType" label="证件类型" width="80" align="center"></el-table-column>
-
-        <el-table-column prop="address" label="证件地址" align="center"></el-table-column>
-        <el-table-column prop="operation" label="操作" align="center" width="80">
+      <el-table :data="BookingTableData" style="width: 100%" border stripe>
+        <el-table-column type="index" width="30"  align="center"></el-table-column>
+        <el-table-column prop="OddNumber" label="预订单号" width="100" align="center"></el-table-column>
+        <el-table-column prop="PersonType" label="客户类型" width="80" align="center"></el-table-column>
+        <el-table-column prop="GroupName" label="团体名称" width="80" align="center"></el-table-column>
+        <el-table-column prop="Subscriber" label="预订人" width="80" align="center"></el-table-column>
+        <el-table-column prop="Telphone" label="联系电话" align="center"></el-table-column>
+        <el-table-column prop="YuDaoTime" label="预到时间" align="center"></el-table-column>
+        <el-table-column prop="YuLiTime" label="预离时间" align="center"></el-table-column>
+        <el-table-column prop="RoomType" label="房间类型" width="80" align="center"></el-table-column>
+        <el-table-column prop="Sum" label="数量" width="50" align="center"></el-table-column>
+        <el-table-column prop="PayFor" label="预付方式" width="80" align="center"></el-table-column>
+        <el-table-column prop="ParForPrice" label="预付金额" width="80" align="center"></el-table-column>
+        <el-table-column prop="CardNum" label="会员卡号" align="center"></el-table-column>
+        <el-table-column prop="Native" label="国籍" width="60" align="center"></el-table-column>
+        <el-table-column prop="OperationTime" label="操作时间" align="center"></el-table-column>
+        <el-table-column prop="Remarks" label="备注" align="center"></el-table-column>
+        <el-table-column prop="OperationPerson" label="操作员" width="80" align="center"></el-table-column>
+        <el-table-column label="操作" align="center" width="150">
           <template>
-            <el-button type="text" size="small">删除</el-button>
+            <el-button type="danger" size="small">取消</el-button>
+            <el-button type="primary" size="small">入住</el-button>
           </template>
         </el-table-column>
       </el-table>
+
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+      ></el-pagination>
     </el-main>
   </el-container>
 </template>
@@ -71,16 +84,72 @@ export default {
         name: "",
         tel: ""
       },
-    BookingTableData: [
+      BookingTableData: [
         {
-          roomNum: 8102,
-          name: "张三",
-          sex: "男",
-          cardType: "身份证",
-          address: "安徽省合肥市滨湖新区西藏路"
+          OddNumber: "yd123456",
+          PersonType: "散客",
+          GroupName: "无",
+          Subscriber: "张三",
+          Telphone: "18256032124",
+          YuDaoTime: "2020-04-12 13:12",
+          YuLiTime: "2020-04-13 13:12",
+          RoomType: "三人间",
+          Sum: 1,
+          PayFor: "现金",
+          ParForPrice: "2000",
+          CardNum: "NO.123456789",
+          Native: "中国",
+          OperationTime: "2020-04-13 13:12",
+          Remarks: "无",
+          OperationPerson: "小吴"
+        },
+        {
+          OddNumber: "yd123456",
+          PersonType: "散客",
+          GroupName: "无",
+          Subscriber: "张三",
+          Telphone: "18256032124",
+          YuDaoTime: "2020-04-12 13:12:12",
+          YuLiTime: "2020-04-13 13:12:12",
+          RoomType: "三人间",
+          Sum: 1,
+          PayFor: "现金",
+          ParForPrice: "2000",
+          CardNum: "NO.123456789",
+          Native: "中国",
+          OperationTime: "2020-04-13 13:12:12",
+          Remarks: "无",
+          OperationPerson: "小吴"
+        },
+        {
+          OddNumber: "yd123456",
+          PersonType: "散客",
+          GroupName: "无",
+          Subscriber: "张三",
+          Telphone: "18256032124",
+          YuDaoTime: "2020-04-12 13:12:12",
+          YuLiTime: "2020-04-13 13:12:12",
+          RoomType: "三人间",
+          Sum: 1,
+          PayFor: "现金",
+          ParForPrice: "2000",
+          CardNum: "NO.123456789",
+          Native: "中国",
+          OperationTime: "2020-04-13 13:12:12",
+          Remarks: "无",
+          OperationPerson: "小吴"
         }
-      ]
+      ],
+      currentPage4: 4
     };
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
   }
 };
 </script>
@@ -88,6 +157,10 @@ export default {
 <style lang="less" scoped>
 .el-main {
   background: #fff;
+  .el-pagination {
+    margin-top: 30px;
+    float: right;
+  }
 }
 </style>
 
--- src/views/MemberManage/MemberManage.vue
+++ src/views/MemberManage/MemberManage.vue
@@ -1,8 +1,199 @@
 <template>
-  <div>
-   会员管理
-  </div>
+  <!-- 住客查询 -->
+  <el-container>
+    <el-main>
+      <el-form  :model="BookingForm"  >
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="预订时间段：">
+              <el-date-picker
+                v-model="BookingForm.value1"
+                type="datetimerange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+              ></el-date-picker>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item>
+              <el-input
+                v-model="BookingForm.name"
+                placeholder="请输入姓名/身份证号/手机号"
+                clearable
+                :style="{width: '100%'}"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        
+          <el-col :span="2">
+            <el-button type="primary">查询</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+
+      <div class="btn">
+        <el-row>
+          <el-button type="primary">增加</el-button>
+          <el-button type="warning">修改</el-button>
+        </el-row>
+      </div>
+
+      <el-table :data="BookingTableData" style="width: 100%" border stripe>
+        <el-table-column type="index" width="30" align="center"></el-table-column>
+        <el-table-column prop="OddNumber" label="预订单号" width="100" align="center"></el-table-column>
+        <el-table-column prop="PersonType" label="客户类型" width="80" align="center"></el-table-column>
+        <el-table-column prop="GroupName" label="团体名称" width="80" align="center"></el-table-column>
+        <el-table-column prop="Subscriber" label="预订人" width="80" align="center"></el-table-column>
+        <el-table-column prop="Telphone" label="联系电话" align="center"></el-table-column>
+        <el-table-column prop="YuDaoTime" label="预到时间" align="center"></el-table-column>
+        <el-table-column prop="YuLiTime" label="预离时间" align="center"></el-table-column>
+        <el-table-column prop="RoomType" label="房间类型" width="80" align="center"></el-table-column>
+        <el-table-column prop="Sum" label="数量" width="50" align="center"></el-table-column>
+        <el-table-column prop="PayFor" label="预付方式" width="80" align="center"></el-table-column>
+        <el-table-column prop="ParForPrice" label="预付金额" width="80" align="center"></el-table-column>
+        <el-table-column prop="CardNum" label="会员卡号" align="center"></el-table-column>
+        <el-table-column prop="Native" label="国籍" width="60" align="center"></el-table-column>
+        <el-table-column prop="OperationTime" label="操作时间" align="center"></el-table-column>
+        <el-table-column prop="Remarks" label="备注" align="center"></el-table-column>
+        <el-table-column prop="OperationPerson" label="操作员" width="80" align="center"></el-table-column>
+        <el-table-column label="操作" align="center" width="150">
+          <template>
+            <el-button type="primary" size="small">充值</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+      ></el-pagination>
+    </el-main>
+  </el-container>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      BookingForm: {
+        value1: [
+          new Date(2000, 10, 10, 10, 10),
+          new Date(2000, 10, 11, 10, 10)
+        ],
+        name: "",
+        tel: ""
+      },
+      BookingTableData: [
+        {
+          OddNumber: "yd123456",
+          PersonType: "散客",
+          GroupName: "无",
+          Subscriber: "张三",
+          Telphone: "18256032124",
+          YuDaoTime: "2020-04-12 13:12",
+          YuLiTime: "2020-04-13 13:12",
+          RoomType: "三人间",
+          Sum: 1,
+          PayFor: "现金",
+          ParForPrice: "2000",
+          CardNum: "NO.123456789",
+          Native: "中国",
+          OperationTime: "2020-04-13 13:12",
+          Remarks: "无",
+          OperationPerson: "小吴"
+        },
+        {
+          OddNumber: "yd123456",
+          PersonType: "散客",
+          GroupName: "无",
+          Subscriber: "张三",
+          Telphone: "18256032124",
+          YuDaoTime: "2020-04-12 13:12:12",
+          YuLiTime: "2020-04-13 13:12:12",
+          RoomType: "三人间",
+          Sum: 1,
+          PayFor: "现金",
+          ParForPrice: "2000",
+          CardNum: "NO.123456789",
+          Native: "中国",
+          OperationTime: "2020-04-13 13:12:12",
+          Remarks: "无",
+          OperationPerson: "小吴"
+        },
+        {
+          OddNumber: "yd123456",
+          PersonType: "散客",
+          GroupName: "无",
+          Subscriber: "张三",
+          Telphone: "18256032124",
+          YuDaoTime: "2020-04-12 13:12:12",
+          YuLiTime: "2020-04-13 13:12:12",
+          RoomType: "三人间",
+          Sum: 1,
+          PayFor: "现金",
+          ParForPrice: "2000",
+          CardNum: "NO.123456789",
+          Native: "中国",
+          OperationTime: "2020-04-13 13:12:12",
+          Remarks: "无",
+          OperationPerson: "小吴"
+        }
+      ],
+      currentPage4: 4,
+    };
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.el-main {
+  background: #fff;
+  .el-pagination {
+    margin-top: 30px;
+    float: right;
+  }
+  .btn {
+    display: flex;
+    justify-content: flex-start;
+    margin-bottom: 20px;
+  }
+}
+</style>
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+   
 
 
 
