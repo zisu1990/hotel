670ed27c7f42d30373692f57f8c8324e364ab16c
lili <865609424@qq.com>
2020-11-30 15:07:11 +0800

    zz

--- src/views/MemberSetting/MemberSetting.vue
+++ src/views/MemberSetting/MemberSetting.vue
@@ -19,24 +19,23 @@
           </el-col>
         </el-row>
       </el-form>
-      <div class="btn">
+      <!-- <div class="btn">
         <el-row>
           <el-button type="primary" @click="AddDialogVisible=true">增加</el-button>
         </el-row>
-      </div>
+      </div>-->
       <el-table :data="LogTableData" style="width: 100%" border stripe>
         <el-table-column type="index" width="80" align="center"></el-table-column>
 
         <el-table-column prop="Name" label="会员等级名称" width="120" align="center"></el-table-column>
         <el-table-column prop="fanwei" label="等级前置条件（积分范围）" align="center"></el-table-column>
-        <el-table-column prop="content" label="优惠内容" align="center"></el-table-column>
+        <el-table-column prop="company" label="所属企业" align="center"></el-table-column>
         <el-table-column prop="discount" label="享受折扣" align="center"></el-table-column>
         <el-table-column prop="time" label="录入时间" align="center"></el-table-column>
-        <el-table-column prop="company" label="所属企业" align="center"></el-table-column>
         <el-table-column prop="youhui" label="优惠内容" align="center"></el-table-column>
         <el-table-column label="操作" align="center" width="150">
           <template>
-            <el-button type="primary" size="small">修改</el-button>
+            <el-button type="primary" size="small" @click="editdialogVisible=true">设置</el-button>
             <el-button type="danger" size="small">删除</el-button>
           </template>
         </el-table-column>
@@ -52,34 +51,58 @@
         :total="400"
       ></el-pagination>
 
-      <el-dialog
-        title="新增会员等级"
-        :model="AddForm"
-        :visible.sync="AddDialogVisible"
-        width="40%"
-        label-width="80px"
-      >
-        <el-form v-model="AddForm">
+      <el-dialog title="新增会员等级" :model="AddForm" :visible.sync="editdialogVisible" width="30%">
+        <el-form v-model="AddForm" label-width="120px">
           <el-form-item label="会员等级名称：">
-            <el-input v-model="AddForm.name"></el-input>
+            <el-input v-model="AddForm.name" disabled></el-input>
           </el-form-item>
 
-          <el-form-item label="物件价格：">
-            <el-input v-model="AddForm.price"></el-input>
+          <el-form-item label="会员等级名称：">
+            <el-input v-model="AddForm.level" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="所属企业：">
+            <el-input v-model="AddForm.company" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="享受折扣：">
+            <el-row>
+              <el-col :span="6">
+                <el-input v-model="AddForm.discount"></el-input>
+              </el-col>
+              <el-col :span="2">
+                <span>折</span>
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <el-form-item label="充值优惠：">
+            <el-row>
+              <el-col :span="2">
+                <span>充</span>
+              </el-col>
+              <el-col :span="6">
+                <el-input v-model="AddForm.chong"></el-input>
+              </el-col>
+              <el-col :span="2">
+                <span>送</span>
+              </el-col>
+              <el-col :span="6">
+                <el-input v-model="AddForm.song"></el-input>
+              </el-col>
+              <el-col :span="2">
+                <i class="el-icon-remove"></i>
+              </el-col>
+              <el-col :span="2">
+                <i class="el-icon-circle-plus"></i>
+              </el-col>
+            </el-row>
           </el-form-item>
 
           <el-form-item label="录入时间：">
-            <el-date-picker
-              type="date"
-              placeholder="选择日期"
-              v-model="AddForm.time"
-              style="width: 100%;"
-            ></el-date-picker>
+            <el-input v-model="AddForm.time"></el-input>
           </el-form-item>
         </el-form>
         <span slot="footer" class="dialog-footer">
-          <el-button @click="AddDialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="AddDialogVisible = false">确 定</el-button>
+          <el-button @click="editdialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="editdialogVisible = false">确 定</el-button>
         </span>
       </el-dialog>
     </el-main>
@@ -98,38 +121,40 @@ export default {
         {
           Name: "钻石会员",
           fanwei: "0-2000",
-          content: "充2000送500",
           discount: "9折",
-          time: "2020-04-13 13：13",
+          time: "2020-04-13 13:13",
+          company: "安徽轻游信息技术有限公司",
+          youhui: "充2000送500;充1000送50"
+        },
+        {
+          Name: "白金会员",
+          fanwei: "0-2000",
+
+          discount: "9折",
+          time: "2020-04-13 13:13",
           company: "安徽轻游信息技术有限公司",
           youhui: "充2000送500"
         },
         {
-          Name: "钻石会员",
+          Name: "黑钻会员",
           fanwei: "0-2000",
-          content: "充2000送500",
+
           discount: "9折",
-          time: "2020-04-13 13：13",
-          company: "安徽轻游信息技术有限公司",
-          youhui: "充2000送500"
-        },
-        {
-          Name: "钻石会员",
-          fanwei: "0-2000",
-          content: "充2000送500",
-          discount: "9折",
-          time: "2020-04-13 13：13",
+          time: "2020-04-13 13:13",
           company: "安徽轻游信息技术有限公司",
           youhui: "充2000送500"
         }
       ],
       currentPage4: 4,
       AddForm: {
-        name: "",
-        price: "",
-        time: ""
+        name: "钻石会员",
+        level: "2001-4000分",
+        company: "安徽轻游信息技术有限公司",
+        chong: "",
+        song: "",
+        discount: ""
       },
-      AddDialogVisible: false
+      editdialogVisible: false
     };
   },
   methods: {
@@ -158,6 +183,13 @@ export default {
   .el-form {
     /deep/.el-select {
       width: 300px;
+    }
+  }
+  .el-dialog {
+    .el-form {
+      /deep/.el-select {
+        width: 415px;
+      }
     }
   }
 }
