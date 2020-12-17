ce20461a0dedac2c70a088108ae4a7537361f07d
lili <865609424@qq.com>
2020-12-17 12:22:02 +0800

    1

--- src/views/BillSetting/BillSetting.vue
+++ src/views/BillSetting/BillSetting.vue
@@ -8,73 +8,87 @@
           </el-row>
 
           <el-row align="middle" type="flex" justify="start">
-            1、
+            1、当天
             <el-time-picker
               is-range
-               arrow-control
+              arrow-control
               v-model="formDevice.setTime"
               range-separator="至"
               start-placeholder="开始时间"
               end-placeholder="结束时间"
               placeholder="选择时间范围"
-              @change="getTime()"
-            ></el-time-picker>时入住客人加
-            <el-input v-model="formDevice.oneSelectPrice" clearable></el-input>收天房费；
+            ></el-time-picker>时入住客人加收
+            <el-input v-model="formDevice.yz_date" clearable></el-input>天房费；
           </el-row>
 
           <el-row align="middle" type="flex" justify="start">
             2、 退房超时
             <el-time-picker
-              v-model="formDevice.twoSelectDate"
+              
+              v-model="formDevice.tfend_time1"
               placeholder="请选择时间"
               style="width: 180px"
             ></el-time-picker>每超过1小时加
-            <el-input v-model="formDevice.twoSelectPrice" clearable></el-input>元房费；退房超过
+            <el-input v-model="formDevice.tf_money1" clearable></el-input>元房费；退房超过
             <el-time-picker
-              v-model="formDevice.twoSelectDateNext"
+              
+              v-model="formDevice.tfend_time2"
               placeholder="请选择时间"
               style="width: 180px"
             ></el-time-picker>加收
-            <el-input v-model="formDevice.twoSelectPriceNext" clearable></el-input>天房费；
+            <el-input v-model="formDevice.tf_date" clearable></el-input>天房费；
           </el-row>
 
           <el-row align="middle" type="flex" justify="start">
-            3、当客人预交款不足
-            <el-input v-model="formDevice.threeSelectDate" clearable></el-input>天房费时，房态表图标提示
+            3、当住客离店时间与当前时间的时间差小于
+            <el-input v-model="formDevice.datecount" clearable></el-input>分钟时，消息提醒前台查看；
           </el-row>
 
           <el-row align="middle" type="flex" justify="start">
-            4、会员入住退房时间为
+            4、会员入住退房时间点超过
             <el-time-picker
-              v-model="formDevice.fourSelectDate"
+            
+              v-model="formDevice.membertf_end_time1"
               placeholder="请选择时间"
               style="width: 180px"
-            ></el-time-picker>3小时内每超过1小时加
-            <el-input v-model="formDevice.fourSelectPrice" clearable></el-input>元房费；退房超过
+            ></el-time-picker>，每小时加收
+            <el-input v-model="formDevice.membertf_tf_money1" clearable></el-input>元房费；退房超过
             <el-time-picker
-              v-model="formDevice.fourSelectDateNext"
+            
+              v-model="formDevice.membertf_end_time2"
               placeholder="请选择时间"
               style="width: 180px"
             ></el-time-picker>加收
-            <el-input v-model="formDevice.fourSelectPriceNext" clearable></el-input>元房费；
+            <el-input v-model="formDevice.membertf_tf_date" clearable></el-input>天房费；
           </el-row>
 
           <el-row align="middle" type="flex" justify="start">
-            5、酒店LOGO
-            <el-button type="primary">点击上传</el-button>
+            5、酒店LOGO：
+            <!-- <el-button type="primary">点击上传</el-button> -->
+            <el-upload style="border:1px dashed   #ddd"
+              class="avatar-uploader"
+              :multiple="false"
+              action="http://www.api.vip/api/setting/upload"
+              :show-file-list="false"
+              :before-upload="beforeAvatarUpload"
+              :http-request="selectPicUpload"
+            >
+              <img v-if="imageUrl" :src="imageUrl" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon">上传</i>
+            </el-upload>
           </el-row>
 
           <el-row align="middle" type="flex" justify="start">
-            6、酒店总台电话
+            6、酒店总台电话：
             <el-input style="width: 180px" clearable v-model="formDevice.tel"></el-input>
           </el-row>
 
           <el-row align="middle" type="flex" justify="start">
             7、酒店业务系统自动夜审时间设置
-            <el-time-picker v-model="formDevice.nightAuditorDate" placeholder="请选择时间"></el-time-picker>
+            <el-time-picker v-model="formDevice.ys_time"  placeholder="请选择时间"></el-time-picker>
           </el-row>
 
-          <el-row align="middle" type="flex" justify="start">8、酒店业务员班次设置</el-row>
+          <el-row align="middle" type="flex" justify="start">8、酒店业务员班次设置:</el-row>
 
           <el-row>
             <el-col>
@@ -84,16 +98,17 @@
                 :cell-style="{ textAlign: 'center' }"
                 style="width: 100%"
               >
-                <el-table-column prop="classes" label="班次名">
+                <el-table-column prop="name" label="班次名">
                   <template v-slot="scope">
-                    <el-input style="width: 200px" v-model="scope.row.classes" placeholder="请输入班次"></el-input>
+                    <el-input style="width: 200px" v-model="scope.row.name" placeholder="请输入班次"></el-input>
                   </template>
                 </el-table-column>
                 <el-table-column prop="date" label="班次工作时段">
                   <template v-slot="scope">
                     <el-time-picker
                       is-range
-                      v-model="scope.row.date"
+                      @change="getBanTime($event)"
+                      v-model="scope.row.time"
                       range-separator="至"
                       start-placeholder="开始时间"
                       end-placeholder="结束时间"
@@ -114,7 +129,7 @@
           <el-row class="footer" type="flex" justify="center">
             <el-col>
               <el-button>取消</el-button>
-              <el-button type="primary">保存</el-button>
+              <el-button type="primary" @click="submitForm">保存</el-button>
             </el-col>
           </el-row>
         </el-col>
@@ -124,48 +139,102 @@
 </template>
 <script>
 import { getAllTime, getDayTime } from "@/utils/moment.js";
+import { SetLogo,SetBill,GetInfo} from "@/api/BillSetting";
 export default {
   data() {
     return {
       formDevice: {
         setTime: "",
-        oneSelectPrice: "",
-        twoSelectPrice: "",
-        twoSelectDate: "",
-        twoSelectDateNext: "",
-        twoSelectPriceNext: "",
-        threeSelectDate: "",
-        fourSelectDate: "",
-        fourSelectPrice: "",
-        fourSelectDateNext: "",
-        fourSelectPriceNext: "",
-        tel: "123456789",
-        nightAuditorDate: ""
+        yz_date: "",
+        tfend_time1: "", //非会员退房时间
+        tf_money1: "",
+        tfend_time2: "",
+        tf_date: "",
+        datecount: "",
+        membertf_end_time1: "",
+        membertf_tf_money1: "",
+        membertf_end_time2: "",
+        membertf_tf_date: "",
+        tel: "",
+        ys_time: "",
       },
-      start_time:'',
-      end_time:'',
+
+      // 夜间入住时间段
+      start_time: "",
+      end_time: "",
+
       tableData: [
         {
-          classes: "123",
-          date: ""
+          name: "早班",
+          time: ""
         }
-      ]
+      ],
+      imageUrl: "",
+      logo:'',
+      // 值班时间段
+      banTime:'',
+
+      // 值班信息
+      ban_info:'早班,7:30-15:30;晚班,15:30-23:30;夜班,23:30-7:30',
     };
+  },
+  created(){
+    this.GetBillInfo()
   },
   methods: {
 
-      //获取选中的时间
-      getTime(){
-        this.start_time=getAllTime(this.formDevice.setTime[0]);
-        this.end_time=getAllTime(this.formDevice.setTime[1]);
-        console.log(this.start_time)
-        console.log(this.end_time)
-      },
+    // 获取计费详情
+    GetBillInfo(){
+      GetInfo().then(res =>{
+        res = typeof res == "string" ? JSON.parse(res) : res;
+        console.log(res,'计费详情')
+        if(res.code===0){
+          this.formDevice=res.data
+          this.imageUrl='https://api.anhuiqingyou.com/uploads/'+res.data.logo;
+          this.tableData=res.data.ban_info
+        }else {
+          this.message("error", res.message);
+        }
+
+      })
+    },
+
+
+    // 上传logo
+    selectPicUpload(f) {
+      let fd = new FormData();
+      fd.append("file", f.file);
+      SetLogo(fd).then(res => {
+        res = typeof res == "string" ? JSON.parse(res) : res;
+        console.log(res);
+        if (res.code == 0) {
+          this.imageUrl='https://api.anhuiqingyou.com/uploads/'+res.data[0][0];
+          this.logo=res.data[0][0]
+        } else {
+          this.message("error", res.message);
+        }
+      });
+    },
+
+  
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isPNG = file.type === "image/png";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG && !isPNG) {
+        this.$message.error("上传头像图片只能是 JPG/png格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG || isPNG && isLt2M ;
+    },
 
 
 
+    // 增加
     addClass(v) {
-    
       let tableData = this.tableData;
       if (tableData.length >= 3) {
         return this.$message({
@@ -174,10 +243,12 @@ export default {
         });
       }
       tableData.push({
-        classes: "123",
-        date: ""
+        name: "早班",
+        time: ""
       });
     },
+
+    // 减少
     reduceClass(v) {
       let tableData = this.tableData;
       if (tableData.length <= 1) {
@@ -187,7 +258,41 @@ export default {
         });
       }
       tableData.splice(v.$index, 1);
-    }
+    },
+
+
+
+      // 提交表单
+      submitForm(){
+          let parmas={
+            yzstart_time:getAllTime(this.formDevice.setTime[0]).substring(11),
+            yzend_time:getAllTime(this.formDevice.setTime[1]).substring(11),
+            yz_date:this.formDevice.yz_date,
+            tfend_time1:getAllTime(this.formDevice.tfend_time1).substring(11),
+            tf_money1:this.formDevice.tf_money1,
+            tfend_time2:getAllTime(this.formDevice.tfend_time2).substring(11),
+            tf_date:this.formDevice.tf_date,
+            datecount:this.formDevice.datecount,
+            membertf_end_time1:getAllTime(this.formDevice.membertf_end_time1).substring(11),
+            membertf_tf_money1:this.formDevice.membertf_tf_money1,
+            membertf_end_time2:getAllTime(this.formDevice.membertf_end_time2).substring(11),
+            membertf_tf_date:this.formDevice.membertf_tf_date,
+            ban_info:this.ban_info,
+            logo:this.logo,
+            tel:this.formDevice.tel,
+            ys_time:getAllTime(this.formDevice.ys_time).substring(11),
+          }
+         SetBill(parmas).then(res => {
+           res = typeof res == "string" ? JSON.parse(res) : res;
+          console.log(res)
+          if (res.code === 0) {
+           this.message("success", res.message)
+          } else {
+            this.message("error", res.message);
+          }
+        });
+      }
+
   }
 };
 </script>
@@ -215,6 +320,31 @@ export default {
   margin-top: 20px;
   .el-button {
     width: 100px;
+  }
+}
+.el-upload {
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
   }
 }
 </style>
